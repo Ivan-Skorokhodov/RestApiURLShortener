2c3b0612bb3a2f386237c514ac17770a4ed28474
Ivan-Skorokhodov <dreikonline5554545@gmail.com>
2024-08-06 21:38:54 +0300

    add configuration

---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+
+type Config struct {
+	Env string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer`yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address string `yaml:"address" env-default:"0.0.0.0:8082"`
+	Timeout time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+    if err := godotenv.Load("local.env"); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+	
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("config file %s does not exist", configPath)
+	}
+	
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal("cannot read %s config", err)
+	}
+
+	return &cfg
+}
